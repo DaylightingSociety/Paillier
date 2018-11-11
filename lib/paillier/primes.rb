bf2fae8a969229395752e86d59321c4806c604b3
Taylor Fox Dahlin <tfdahlin@gmail.com>
2018-11-10 17:30:24 -0800

    Changed primality test from Little Fermat to Miller-Rabin, updated gemspec, added a couple test cases, + some misc edits.

--- lib/paillier/primes.rb
+++ lib/paillier/primes.rb
@@ -10,32 +10,36 @@ module Paillier
 			return int.to_s(2).length
 		end
 
-		def self.defaultK(bits)
-			double = bits * 2
-			return (40 > double) ? 40 : double
-		end
+        # This is an implementation of the Rabin-Miller primality test.
+        # Previous versions used Little Fermat, but that is not effective
+        # in all cases; specifically, it can be thwarted by Carmichael 
+        # numbers. We use 50 rounds as the default, in order to get a certainty
+        # of 2^-100 that we have found a prime. This implementation is adapted
+        # from https://rosettacode.org/wiki/Miller-Rabin_primality_test#Ruby
+        def self.probabilisticPrimeTest(target, k=50)
+            d = target-1
+            s = 0
+            while d % 2 == 0
+                d /= 2
+                s += 1
+            end
+            k.times do
+                a = 2 + rand(target-4)
+                x = a.to_bn.mod_exp(d, target)
+                next if x == 1 || x == target-1
+                for r in (1..s - 1)
+                    x = x.to_bn.mod_exp(2, target)
+                    return false if x == 1
+                    break if x == target - 1
+                end
+                return false if x != target-1
+            end
+            return true # probs prime
+        end
 
-		# This is based on the Wikipedia article on the Fermat primality test
-		# returns true if probably prime, false if definitely composite
-		def self.probabilisticPrimeTest(target, k)
-			for _ in (1 .. k)
-				a = rand(2 .. (target-2))
-				# We want to run "x = (a ** target-1) % target", but the values
-				# are huge. Instead we call out to openssl and do it with mod_exp
-				mod = a.to_bn.mod_exp(target-1, target)
-				if( mod != 1 )
-					return false # Def composite
-				end
-			end
-			return true # probs prime
-		end
-
-		def self.isProbablyPrime?(possible, k=nil)
+		def self.isProbablyPrime?(possible, k=50)
 			if( possible == 1 )
 				return true
-			end
-			if( k.nil? )
-				k = defaultK(bitLength(possible))
 			end
 			for i in SmallPrimes
 				if( possible == i )
@@ -59,14 +63,10 @@ module Paillier
 		end
 
 		# Get a random prime of appropriate length
-		def self.generatePrime(bits, k=nil)
+		def self.generatePrime(bits, k=50)
 			if( bits < 8 )
 				raise "Bits less than eight!"
 			end
-			if( k == nil )
-				k = defaultK(bits)
-			end
-
 			while( true )
 				lowerBound = (2 ** (bits-1) + 1)
 				size = ((2 ** bits) - lowerBound)
@@ -82,7 +82,7 @@ module Paillier
 				raise "Bits less than eight!"
 			end
 
-			# If we find a number not coprome to n then finding `p` and `q` is trivial.
+			# If we find a number not coprime to n then finding `p` and `q` is trivial.
 			# This will almost never happen for keys of reasonable size, so if
 			# `coprime_to` is big enough we won't bother running the expensive test.
 			no_test_needed = false
--- paillier.gemspec
+++ paillier.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
 	s.name        = 'paillier'
-	s.version     = '1.1.0'
-	s.date        = '2017-09-13'
+	s.version     = '1.2.0'
+	s.date        = '2018-11-10'
 	s.summary     = "Paillier Homomorphic Cryptosystem"
 	s.description = "An implementation of Paillier homomorphic addition public key system"
 	s.authors     = ["Daylighting Society"]
